267bdf54ad8e34950d4ff5dba93370fe94c11560
Susanna Lakey <susannalakey@gmail.com>
2021-02-25 12:49:56 -0800

    updating index.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./db');
+const db = require('./index.js');
 
 const app = express();
 
