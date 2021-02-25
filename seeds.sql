d581202aff853f8a984025c43c488fb27454e85e
Susanna Lakey <susannalakey@gmail.com>
2021-02-25 12:43:06 -0800

    updating

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+insert into restaurants (name, location, price_range) values('Olive Garden', 'New York', 3);
+insert into restaurants (name, location, price_range) values('McDonalds', 'Las Vegas', 2);
+insert into restaurants (name, location, price_range) values('California Pizza Kitchen', 'Los Angeles', 5);
+
+insert into reviews (restaurant_id, name, review, rating) values (1, 'Bob', 'Great Food!', 5);
+insert into reviews (restaurant_id, name, review, rating) values (2, 'Sally', 'Not great', 2);
+insert into reviews (restaurant_id, name, review, rating) values (3, 'Fred', 'Loved the food!', 5);
