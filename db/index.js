4eec3602aa52da6349aa46eb92f5c5a7d7ed0cbe
Susanna Lakey <susannalakey@gmail.com>
2021-02-27 15:30:35 -0800

    updated connection string for heroku

--- db/index.js
+++ db/index.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
 };
 const pool = new Pool(devConfig);
 
